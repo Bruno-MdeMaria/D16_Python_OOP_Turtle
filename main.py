01f1a4bc445b7d68b71edfa29e4391a4476a9ae1
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-08-16 00:17:34 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# import turtle - PODEMOS IMPORTAR DESTA FORMA E DAI PRECIISAMOS IMPORTAR A CLASSE COMO A FUNÇÃO ABAIXO:
+
+# samy = turtle.Turtle()
+
+# OU ASSIM:
+
+
+from turtle import Screen, Turtle
+
+samy = Turtle()
+samy.shape("turtle")
+samy.color("coral","green")
+
+my_screnn = Screen()
+my_screnn.exitonclick()
