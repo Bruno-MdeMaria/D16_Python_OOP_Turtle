17e4e57269bc77defe5f5572d6f325a647f00214
Pricila santos <brunomartinsdemaria@gmail.com>
2022-08-28 09:11:08 -0300

    Update project4.spiral.py

--- project4.spiral.py
+++ project4.spiral.py
@@ -3,7 +3,13 @@ import random
 
 samy = t.Turtle()   #CHAMAR TODOS OS MODOS
 t.colormode(255)
-
+samy.shape("turtle")
+samy.color("dodger blue")
+samy.penup()
+samy.sety(100)
+samy.setx(20-100)
+samy.pendown()
+samy.pensize(10)
 
 def random_cor():
     r = random.randint(0,255)
@@ -12,23 +18,11 @@ def random_cor():
     random_color = (r, g, b)
     return random_color
 
-samy.shape("turtle")
-samy.color("dodger blue")
-
-samy.penup()
-samy.sety(100)
-samy.setx(20-100)
-samy.pendown()
-samy.pensize(10)
-
-#lista_cores = ["medium blue", "lime", "dark magenta", "gold", "orange red", "blue violet", "dark green", "red"]
-lista_direcoes = [0, 90, 180, 270]
 
 
-for _ in range(50):
-    samy.color(random_cor())
-    samy.forward(30)
-    samy.setheading(random.choice(lista_direcoes))
+
+
+
 
 
 
