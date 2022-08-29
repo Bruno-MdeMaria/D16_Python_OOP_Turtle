15793463a3c3b8105e27c9780ce5a3e9833b42e1
Pricila santos <brunomartinsdemaria@gmail.com>
2022-08-29 00:30:27 -0300

    Update project4.spiral.py
    Acrescentando comentários e simplificado o setheading (inclinação da t.)

--- project4.spiral.py
+++ project4.spiral.py
@@ -13,19 +13,19 @@ samy.pendown()
 samy.pensize()
 
 
-def random_cor():
-    r = random.randint(0,255)
+#um espirografico:
+
+def random_cor():                    #função para definir uma cor aleatória baseada em RGB
+    r = random.randint(0,255)        #defini as cores red, green e blue de 0 a 255 como no HTML/CSS
     g = random.randint(0,255)
     b = random.randint(0,255)
-    random_color = (r, g, b)
-    return random_color
+    random_color = (r, g, b)        #variavel recebe uma tuble
+    return random_color             #a função retorna a variável.
 
-for _ in range(100):
-    samy.color(random_cor())
-    samy.circle(100)
-    posicao = samy.heading()
-    samy.setheading(posicao +5)
-
+for _ in range(100):                        #um laço de 100 voltas
+    samy.color(random_cor())                #chamar a função random_cor() no método t.color()    
+    samy.circle(100)                        #o método circle com a quantidade de passos(tamanho)
+    samy.setheading(samy.heading()+5)       #é o indice de inlinação da t. o heading recebe a posição atual da inclinação e acrescenta +5.
 
 
 
